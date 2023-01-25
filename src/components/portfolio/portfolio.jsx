860a85dbda6b5a175543f5ee8a3c2ca7cb5a6b61
shrCreation <shrhossain786@gmail.com>
2023-01-25 18:45:34 +0600

    initial commitinitial commit

---
+++ src/components/portfolio/portfolio.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './portfolio.cssw';
+
+const portfolio = () => {
+  return <div>portfolio</div>;
+};
+
+export default portfolio;
---
+++ src/components/testimonials/testimonials.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './testimonials.css';
+
+const testimonials = () => {
+  return <div>testimonials</div>;
+};
+
+export default testimonials;
